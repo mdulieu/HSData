28e58ff83af1553bbca133ee533c0d3a0c924007
mdulieu <mdulieu@gmail.com>
2014-12-01 11:45:00 -0500

    token as os variable

--- HSDataGenerator.py
+++ HSDataGenerator.py
@@ -1,10 +1,13 @@
+import os
 import requests
 from numpy import array
+from numpy import zeros
+from numpy import savetxt
 from scipy.cluster.vq import vq, kmeans2, whiten
 from sets import Set
 
 site = "https://www.hackerschool.com"
-access_token = "?access_token=5cc37b8155311e46954f2ed0b78c77dbb7a5d348b6cebe655c147afd20ca9435"
+access_token = "?access_token=" + os.environ.get('HS_API_TOKEN')
 caller_email = "mdulieu@gmail.com"
 
 #Get all the people in the last two batches
@@ -29,19 +32,36 @@ for person in currentPeople:
 	if person['email'] == caller_email:
 		#print person['first_name'], person['last_name']
 		caller_id = person['id']
+		print caller_id
 
 #print caller_id
 #Code if we don't find the caller_id
 
 #list all the skills
 #Clean-up of the skills: We need to extract skills from sentences and put everything in lower cases
+first_name = []
+last_name = []
+people_id = []
+people_dictionary = {}
+
 allSkillsSet = Set([])
 for person in currentPeople:
+	first_name.append(person['first_name'])
+	last_name.append(person['last_name'])
+	people_id.append(person['id'])
+	people_dictionary[person['id']] = person
+
 	for skill in person['skills']:
 		allSkillsSet.add(skill.lower())
 
+
+
 #for skill in allSkills:
 #	print skill
+
+#print first_name
+#print last_name
+#print people_id
 
 allSkills = list(allSkillsSet)
 allSkills = sorted(allSkills)
@@ -50,7 +70,16 @@ allSkills = sorted(allSkills)
 
 #print allSkills
 #for index, skill in enumerate(allSkills):
-	#print(index, skill)
+#	print(index, skill)
 
-for personIndex in range(len(currentPeople)):
-	for skillIndex in range(len(allSkills)):
+#for personIndex in range(len(currentPeople)):
+#	for skillIndex in range(len(allSkills)):
+
+peopleSkills = zeros((len(people_id), len(allSkills)))
+for i, people in enumerate(people_id):
+	for j, skill in enumerate(allSkills):
+		peopleSkills[i,j]  = skill in people_dictionary[people]['skills']
+
+savetxt("HSData.csv", peopleSkills, delimiter=",", fmt = "%5i")
+
+
